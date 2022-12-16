21c451d1a239f1286ad05a4288772825ae913997
camargo <contato@valdeircamargo.com>
2022-12-16 16:18:05 -0300

    Feat: microservice registrado Eureka

--- src/main/java/org/camargovf/WorkerMainApp.java
+++ src/main/java/org/camargovf/WorkerMainApp.java
@@ -1,5 +1,10 @@
 package org.camargovf;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
 public class WorkerMainApp {
     public static void main(String[] args) {
         System.out.println("WorkerMainApp Online");
